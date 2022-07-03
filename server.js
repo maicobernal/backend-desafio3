56dd15e2a772787d761c98a46f799ccedadea390
maicobernal <bernalmaicogmail.com>
2022-07-03 18:00:13 -0300

    Upload del desafio 3

---
+++ server.js
@@ -0,0 +1,57 @@
+//npm init -y; npm i express; echo "node_modules" >> .gitignore; touch index.js
+//import Contenedor from './carrito.js';
+//const {Container} = Contenedor;
+//import {Productos} from './products.js';
+
+const express = require('express')
+const moment = require('moment')
+const Contenedor = require ('./carrito.js')
+
+const app = express()
+const port = 8080
+const server = app.listen(port,()=>{
+        console.log('Servidor corriendo en el puerto: ' + server.address().port)
+    })
+
+server.on('error', (error)=>{
+    console.log(`Error en el servidor ${error}`)
+})
+
+app.get('/', (req, res)=>{
+    res.send(hola)
+})
+
+const hola = `<h1>Bienvenido al escueto Mercado Virtual de Pulgas de elementos obsoletos informáticos</h1>\n
+<p>Para ver los productos disponibles, haga click <a href=/productos>aquí</a></p>\n
+<p><a href = "/productoRandom">Voy a tener suerte</a></p>\n
+<p><a href = "/fyh">Quiero saber la hora</a></p>\n
+<p><a href = "/visitas">Número de visitas</a></p>\n
+`
+
+let visitas = 0
+
+let stock = new Contenedor('productos.txt');
+
+ app.get('/productos', async (req, res)=> {
+    const productos = await stock.getAll();
+    res.send(productos)
+    }
+)
+
+app.get('/productoRandom', async (req, res)=>{
+    let producto = await stock.getRandom()    
+    res.send(producto)
+    }
+)
+
+app.get('/visitas', (req, res)=>{
+    visitas++
+    res.send(`La cantidad de visitas es ${visitas}`)
+    }
+)
+
+app.get('/fyh', (req, res)=>{
+    const fyh = moment().format('YYYY/MM/DD HH:mm:ss')
+    res.send(fyh)
+})
+
